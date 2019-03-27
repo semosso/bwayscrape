33e0c6363d029f1e06b019e42514d628235d6b9c
Vinicius Azambuja <vnazambuja@gmail.com>
2019-03-27 15:04:46 -0400

    added pick and choose functionality

--- bwayscrape/scraper.py
+++ bwayscrape/scraper.py
@@ -2,6 +2,7 @@ import os
 import sys
 import requests
 import bs4
+import re
 
 # baixar imagem do playbill
 def get_playbill(show):
@@ -11,9 +12,23 @@ def get_playbill(show):
   req_search.raise_for_status() # maybe work on Exception message? repeats below
   
   soup_search = bs4.BeautifulSoup(req_search.text, "html.parser") # this parses what has been downloaded
-  show_element = soup_search.select_one(".bsp-list-promo-title > a")
-  show_url = show_element.get("href")
+  show_element = soup_search.select(".bsp-list-promo-title > a")
+  n = len(show_element)
+  
+  if n > 1:
+    lista_limpa = [re.compile("\s+").sub(" ", a.text).strip() for a in show_element] # entender em detalhes depois
+    print("Pick which show (by index):")
 
+    for i in range(n):
+      print(f"{i}. {lista_limpa[i]}")
+
+    choice = int(input("> "))
+    show_url = show_element[choice].get("href")
+
+  else:
+    show_url = show_element.get("href")
+
+  # I could have gotten playbill IMG directly from show_url, but it'd be a lower quality
   req_show = requests.get("http://www.playbill.com" + show_url)
   req_show.raise_for_status()
   
@@ -24,19 +39,16 @@ def get_playbill(show):
   req_bill = requests.get(bill_url) # downloading image
   req_bill.raise_for_status()
 
-  image_file = open(os.path.join("..", "data", f"{show}.jpg"), "wb")
+  image_file = open(os.path.join("..", "data", "playbills", f"{lista_limpa[choice]}.jpg"), "wb")
    
   for chunk in req_bill.iter_content(100000):
     image_file.write(chunk)
   image_file.close()
 
   # TODO:
-  # p1: revisar e melhorar, eliminar duplicidades; show and confirm search results in ln 14
-  # (maybe bsp-list-promo-title na busca geral, mostrando os nomes, seja a resposta);
-  # aprender a usar path direito (não dá pra ficar dependendo de .., precisa ser absolute);
-  # p2: entender "raise_for_status"; better name for file in ln 27; add comments all around;
-  # "pular" quando não houver playbill disponível (ou melhor, procurar Google imagens);
-  # tentar brute force achar site no playbill (e.g., chick flick?); broadway.com para playbills tbm
+  # p1: revisar e melhorar, eliminar duplicidades; aprender a usar path direito;
+  # p2: entender "raise_for_status"; add comments all around; "pular" quando não houver playbill disponível
+  # alternativas: procurar Google imagens; brute force achar site no playbill (e.g., chick flick); broadway.com
 
 show = "+".join(sys.argv[1:])
 get_playbill(show)
