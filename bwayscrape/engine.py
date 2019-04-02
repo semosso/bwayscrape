07ad6e48c3f9b851a8716da4d275ee81a74b25f5
Vinicius Azambuja <45149122+semosso@users.noreply.github.com>
2019-04-02 11:43:00 -0400

    commenting

--- bwayscrape/engine.py
+++ bwayscrape/engine.py
@@ -1,21 +1,21 @@
 import sys
 from pathlib import Path
-import bill_scrape
+import bill_scrape as bs
 
-PROJECT_FOLDER = Path.cwd().parent # should this be globally defined in every module, or is one enough? read on scopes
+PROJECT_FOLDER = Path.cwd().parent
 
 # starteia a porra toda, chamando a busca de playbill
 if len(sys.argv) > 1:
      show = "+".join(sys.argv[1:])
-     show, show_url, indice = bill_scrape.find_site(show, 0)
+     show, show_url, indice = bs.find_site(show, 0)
+     bs.find_playbill(show, show_url, indice)
 else:
     file_CSV = open(f"{PROJECT_FOLDER}/CSV-file.txt", "r").read()
     show_list = file_CSV.split("\n")
 
     for i in show_list:
         show = i
-        show, show_url, indice = bill_scrape.find_site(show, 0)
-
-bill_scrape.find_playbill(show, show_url, indice)
+        show, show_url, indice = bs.find_site(show, 0)
+        bs.find_playbill(show, show_url, indice)
 
 # adicionar outros módulos e funções
