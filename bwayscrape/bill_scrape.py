9ec74892c59ecafda6082a3d7630338289ea9199
Vinicius Azambuja <45149122+semosso@users.noreply.github.com>
2019-04-02 11:37:22 -0400

    commenting

--- bwayscrape/bill_scrape.py
+++ bwayscrape/bill_scrape.py
@@ -3,11 +3,13 @@ import sys
 import requests
 import bs4 
 
+# do I need this? still haven't figured out how to use/work with paths properly
 PROJECT_FOLDER = Path.cwd().parent
 BILL_FOLDER = PROJECT_FOLDER / "data" / "playbills"
 BILL_FOLDER.mkdir(exist_ok=True, parents=True)
 
-# baixar imagem do playbill
+# tries to find a website for the show; if there's no site on Playbill.com, tries alternatives
+# "trying alternatives" is done with a "list index counter"; if it goes up, changes site
 def find_site(show, indice):
   search_source = [
   f"http://www.playbill.com/searchpage/search?q={show}&sort=Relevance&shows=on",
@@ -23,28 +25,28 @@ def find_site(show, indice):
 
   url = search_source[indice]
     
-  req_search = requests.get(url)
-  req_search.raise_for_status()
-  soup_search = bs4.BeautifulSoup(req_search.text, "html.parser")
-  matches = soup_search.select(show_HTML[indice])
+  req_search = requests.get(url) # makes request to URL and downloads/stores everything 
+  req_search.raise_for_status() # checks to see if request was successful (code 200)
+  soup_search = bs4.BeautifulSoup(req_search.text, "html.parser") # parses the whole site
+  matches = soup_search.select(show_HTML[indice]) # searches parsed site for "blocks" 
   
   n = len(matches)
 
-  if n == 0:
+  if n == 0: # i.e., if no site for show was found
     indice += 1
 
     if indice <= 2:
       print(f"Wait a little longer, trying to find the playbill in other sources...")
       find_site(show, indice)
     else:
-      show, show_url, indice = 0, 0, 0
+      show, show_url, indice = 0, 0, 0 # pass these to find_playbill() so it knows no site was found
   
-  elif n == 1:
-    show_url = matches[0].get("href") # SHOULDN'T CHANGE BETWEEN SITES, BUT YOU NEVER KNOW...
+  elif n == 1: # i.e., if it only found one matching show
+    show_url = matches[0].get("href")
     show = matches[0].text.strip()
   
-  else:
-    results = [a.text.strip() for a in matches] # works for playbill, but who knows for other ones...
+  else: # i.e., if multiple (> 1) shows were found, prints a list and lets me choose between them
+    results = [a.text.strip() for a in matches] # takes away all HTML, returns only text
     
     print("Pick which show (by index):")
     for i in range(min(n, 10)):
@@ -52,10 +54,12 @@ def find_site(show, indice):
     
     choice = int(input("> "))
     show = results[choice]
-    show_url = matches[choice].get("href") # SHOULDN'T CHANGE BETWEEN SITES, BUT YOU NEVER KNOW... 
+    show_url = matches[choice].get("href") 
 
   return show, show_url, indice
-  
+
+# finds and downloads the actual playbill IMG on each site
+# alternative sites are searched based on whatever "index" is passed by find_site()
 def find_playbill(show, show_url, indice):
   show_source = [
     f"http://www.playbill.com{show_url}",
@@ -75,7 +79,8 @@ def find_playbill(show, show_url, indice):
     # FIGURE OUT 2
   ]
 
-  if (show == 0): # how to make it match all three params?
+  if (show == 0): # i.e., if no show was found on any site
+                  # how to make it match all three params? don't need it, but it seems sloppy not to 
     print(f"Couldn't find a playbill based on {sys.argv[1:]} keywords :(")
     exit(1)
 
@@ -90,13 +95,14 @@ def find_playbill(show, show_url, indice):
     req_bill = requests.get(bill_url)
     req_bill.raise_for_status()
 
-    image_file = open(f"{BILL_FOLDER}/{show}.jpg", "wb") 
+    image_file = open(f"{BILL_FOLDER}/{show}.jpg", "wb") # "w" (or "a", for that matter) creates if there's nothing there
 
-    for chunk in req_bill.iter_content(100000):
+    for chunk in req_bill.iter_content(100000): # from ABSWP: write in chunks, useful for larger things
       image_file.write(chunk)
     image_file.close()
     print(f"Playbill for '{show}' has been downloaded!")
 
   # TODO:
   # p1: eliminar duplicidades (funções repetidas como em autolog?); aprender a usar path direito;
-  # p2: add comments all around
+  # p2: lista de enderecos no retorno de matches (facilita distinguir entre producoes)
+  # NDA na escolha da lista de matches, resultando em indice += 1
